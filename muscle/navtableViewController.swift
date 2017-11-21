d09a790bf09297aecfb01064826cdc2fa0d07e4b
sasakazu <dropout.college2014@gmail.com>
2017-11-21 19:53:08 +0900

    table base done......

---
+++ muscle/navtableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  navtableViewController.swift
+//  muscle
+//
+//  Created by 笹倉 一也 on 2017/11/21.
+//  Copyright © 2017年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class navtableViewController: UINavigationController , UITableViewDelegate, UITableViewDataSource {
+    
+    let fruits = ["リンゴ", "みかん", "ぶどう"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /// セルの個数を指定するデリゲートメソッド（必須）
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    /// セルに値を設定するデータソースメソッド（必須）
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        
+        // セルに表示する値を設定する
+        cell.textLabel!.text = fruits[indexPath.row]
+        
+        return cell
+    }
+    
+    /// セルが選択された時に呼ばれるデリゲートメソッド
+    private func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
+        print("セル番号：\(indexPath.row) セルの内容：\(fruits[indexPath.row])")
+    }
+}
+
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destinationViewController.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
