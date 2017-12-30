921660975103e0b38eb1bad099f85121861a5614
sasakazu <dropout.college2014@gmail.com>
2017-12-30 19:20:53 +0900

    mm cell section base done.....

---
+++ muscle/muscle-menu/mmMainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  mmMainViewController.swift
+//  muscle
+//
+//  Created by 笹倉 一也 on 2017/12/30.
+//  Copyright © 2017年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class mmMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
---
+++ muscle/muscle-menu/mmSelecteViewController.swift
@@ -0,0 +1,131 @@
+//
+//  mmSelecteViewController.swift
+//  muscle
+//
+//  Created by 笹倉 一也 on 2017/12/30.
+//  Copyright © 2017年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class mmSelecteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    //    セクション
+    let mmSection = ["胸", "腕", "背中"]
+
+    //    胸
+    let mmItemBreast = ["ベンチプレス", "インクライン"]
+    
+    //    腕
+    let mmItemArm = ["ダンベル", "ダンベルカール"]
+    
+    //    背中
+    let mmItemBack = ["チェスと", "手ェ酢と"]
+    
+    //    足
+    let mmItemFoot = ["デットリフト", "デット"]
+    
+    
+    @IBOutlet weak var mmSelecteTableview: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return mmSection.count
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return mmSection[section]
+    }
+
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 0 {
+            return mmItemBreast.count
+        }
+        else if section == 1 {
+            return mmItemBack.count
+        }
+        else if section == 2 {
+            return mmItemBack.count
+        }
+            
+        
+        else if section == 3 {
+            return mmItemFoot.count
+        }
+            
+        else{
+            return 0
+        }
+        
+    
+     
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mmSelecteCell", for: indexPath)
+   
+        if indexPath.section == 0 {
+            
+            cell.textLabel?.text = "\(mmItemBreast[indexPath.row])"
+            
+        } else if indexPath.section == 1 {
+            
+            cell.textLabel?.text = "\(mmItemArm[indexPath.row])"
+            
+        }
+        
+        else if indexPath.section == 2 {
+            
+            cell.textLabel?.text = "\(mmItemBack[indexPath.row])"
+            
+        }
+        
+        else if indexPath.section == 3 {
+            
+            cell.textLabel?.text = "\(mmItemFoot[indexPath.row])"
+            
+        }
+
+        return cell
+    
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    
+    }
+
+    
+    
+    @IBAction func mmOkBtn(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+}
