76e3a9045fbff3a22f78ea6a7527ef3d2974962f
sasakazu <dropout.college2014@gmail.com>
2018-01-04 18:53:02 +0900

    edit delete ban done...............

--- muscle/muscle-menu/mmMainViewController.swift
+++ muscle/muscle-menu/mmMainViewController.swift
@@ -8,13 +8,78 @@
 
 import UIKit
 
-class mmMainViewController: UIViewController {
+class mmMainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    var test = [ "test1", "test2", "test3", "test4"]
 
+    
+    @IBOutlet weak var mmMainTableView: UITableView!
+    @IBOutlet weak var mmMainEditBtn: UIBarButtonItem!
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return test.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mmMainCell", for: indexPath)        
+        cell.textLabel?.text = test[indexPath.row]
+        
+        return cell
+    }
+    
+
+      //  delete スワイプ
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            test.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+
+    //move cell functions
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let mmMainitem = test[sourceIndexPath.row]
+        test.remove(at: sourceIndexPath.row)
+        test.insert(mmMainitem, at: destinationIndexPath.row)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+       
+        
     }
+    
+    
+    @IBAction func mmMainEdtitActionBtn(_ sender: Any) {
+        mmMainTableView.isEditing = !mmMainTableView.isEditing
+            
+        switch mmMainTableView.isEditing {
+        
+            case true:
+                mmMainEditBtn.title = "Done"
+            case false:
+                mmMainEditBtn.title = "Edit"
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
