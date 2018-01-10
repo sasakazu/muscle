213413ce2fb130817f5c7d2f0ad3bda03b2f5fac
sasakazu <dropout.college2014@gmail.com>
2018-01-10 20:30:51 +0900

    time lines base done...

--- muscle/timeline/timeLineViewController.swift
+++ muscle/timeline/timeLineViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class timeLineViewController: UIViewController {
+class timeLineViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+  
     
-    
-    @IBOutlet weak var table: UITableView!
-    
+    let timeLineTitle = [ "titletest1", "titletest2", "titletest3", "titletest4", "titletest5" ]
 
+    let timelineImg = [ "img1", "img2", "img3", "img4"]
+  
+    @IBOutlet weak var timelineTableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timelineTableView.delegate = self
+        timelineTableView.dataSource = self
 
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return timeLineTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "timeLineCell", for: indexPath)
+        
+        
+        // Tag番号 2 で UILabel インスタンスの生成
+        let label2 = cell.viewWithTag(2) as! UILabel
+        label2.text = String(describing: timeLineTitle[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
