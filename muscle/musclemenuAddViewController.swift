416eba5e35d5e149895d3b57ed6ed5204d807c0c
sasakazu <dropout.college2014@gmail.com>
2018-01-30 19:37:47 +0900

    muscle menu base done....

---
+++ muscle/musclemenuAddViewController.swift
@@ -0,0 +1,37 @@
+//
+//  musclemenuAddViewController.swift
+//  muscle
+//
+//  Created by 笹倉 一也 on 2018/01/30.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class musclemenuAddViewController: UIViewController {
+    
+  
+
+    
+    @IBOutlet weak var musclemenuTextField: UITextField!
+    @IBOutlet weak var musclemenuTableView: UITableView!
+    @IBOutlet weak var musclemenuAddBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func musclemenuAddTapped(_ sender: Any) {
+    }
+    
+
+}
